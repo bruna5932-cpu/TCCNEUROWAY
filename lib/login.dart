bf2197faec8d0ad0cbba0aa86e8e78c758669711
bruna5932-cpu <bruna5932@institutoideia.com>
2026-06-12 15:54:19 -0300

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,457 +1,222 @@
 import 'package:flutter/material.dart';
-
 import 'package:neuroway/cadastroconta.dart';
-
 import 'package:neuroway/cadastroempresa.dart';
-
 import 'package:neuroway/menuprincipal.dart';
 
-
-
 class LOGIN extends StatefulWidget {
-
   const LOGIN({super.key});
 
-
-
   @override
-
   State<LOGIN> createState() => LOGINState();
-
 }
 
-
-
 class LOGINState extends State<LOGIN> {
-
   final TextEditingController _emailController = TextEditingController();
-
   final TextEditingController _passwordController = TextEditingController();
 
-
-
   @override
-
   void dispose() {
-
     _emailController.dispose();
-
     _passwordController.dispose();
-
     super.dispose();
-
   }
 
-
-
   @override
-
   Widget build(BuildContext context) {
-
     final screenHeight = MediaQuery.of(context).size.height;
-
     final screenWidth = MediaQuery.of(context).size.width;
-
     final molduraHeight = screenHeight * 0.25;
 
-
-
     return Scaffold(
-
       backgroundColor: const Color(0xFFFFFFFF),
-
       body: Stack(
-
         children: [
-
-          // Conteúdo principal com scroll
-
+          Positioned(
+            top: molduraHeight * 0.5,
+            bottom: molduraHeight * 0.5,
+            left: 0,
+            right: 0,
+            child: Image.asset(
+              'imagem/login.png',
+              width: double.infinity,
+              fit: BoxFit.fill,
+            ),
+          ),
           SafeArea(
-
             child: SingleChildScrollView(
-
               child: ConstrainedBox(
-
                 constraints: BoxConstraints(minHeight: screenHeight),
-
                 child: Padding(
-
                   padding: EdgeInsets.only(
-
                     top: molduraHeight * 0.6,
-
                     bottom: molduraHeight * 0.6,
-
                   ),
-
                   child: Column(
-
                     crossAxisAlignment: CrossAxisAlignment.start,
-
                     children: [
-
-                      // Logo central
-
                       Center(
-
                         child: SizedBox(
-
                           width: screenWidth * 0.55,
-
                           height: screenHeight * 0.18,
-
                           child: Image.network(
-
                             "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/wzMjUWejTS/t552npi8_expires_30_days.png",
-
                             fit: BoxFit.contain,
-
                           ),
-
                         ),
-
                       ),
-
-
-
                       SizedBox(height: screenHeight * 0.03),
-
-
-
-                      // Formulário
-
                       Padding(
-
                         padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.1),
-
                         child: Column(
-
                           children: [
-
                             _buildTextField("E-mail", _emailController),
-
                             SizedBox(height: screenHeight * 0.02),
-
                             _buildTextField("Senha", _passwordController, isPassword: true),
-
                           ],
-
                         ),
-
                       ),
-
-
-
                       SizedBox(height: screenHeight * 0.04),
-
-
-
-                      // Botões Login e Cadastrar
-
                       Row(
-
                         mainAxisAlignment: MainAxisAlignment.center,
-
                         children: [
-
                           ElevatedButton(
-
                             onPressed: () {
-
                               Navigator.push(
-
                                 context,
-
                                 MaterialPageRoute(builder: (context) => const Menuprincipal()),
-
                               );
-
                             },
-
                             style: ElevatedButton.styleFrom(
-
                               backgroundColor: const Color(0xFF98B9A6),
-
                               foregroundColor: Colors.black,
-
                               padding: EdgeInsets.symmetric(
-
                                 horizontal: screenWidth * 0.07,
-
                                 vertical: screenHeight * 0.015,
-
                               ),
-
                               shape: RoundedRectangleBorder(
-
                                 borderRadius: BorderRadius.circular(8),
-
                               ),
-
                               elevation: 2,
-
                             ),
-
                             child: Text(
-
                               "Login",
-
                               style: TextStyle(
-
                                 fontSize: screenWidth * 0.045,
-
                                 fontWeight: FontWeight.bold,
-
                               ),
-
                             ),
-
                           ),
-
-
-
                           SizedBox(width: screenWidth * 0.04),
-
-
-
                           ElevatedButton(
-
                             onPressed: () {
-
                               Navigator.push(
-
                                 context,
-
                                 MaterialPageRoute(builder: (context) => const CriarConta()),
-
                               );
-
                             },
-
                             style: ElevatedButton.styleFrom(
-
                               backgroundColor: const Color(0xFF98B9A6),
-
                               foregroundColor: Colors.black,
-
                               padding: EdgeInsets.symmetric(
-
                                 horizontal: screenWidth * 0.07,
-
                                 vertical: screenHeight * 0.015,
-
                               ),
-
                               shape: RoundedRectangleBorder(
-
                                 borderRadius: BorderRadius.circular(8),
-
                               ),
-
                               elevation: 2,
-
                             ),
-
                             child: Text(
-
                               "Cadastrar",
-
                               style: TextStyle(
-
                                 fontSize: screenWidth * 0.045,
-
                                 fontWeight: FontWeight.bold,
-
                               ),
-
                             ),
-
                           ),
-
                         ],
-
                       ),
-
-
-
                       SizedBox(height: screenHeight * 0.05),
-
-
-
-                      // Botão cadastrar empresa
-
                       Center(
-
                         child: GestureDetector(
-
                           onTap: () {
-
                             Navigator.push(
-
                               context,
-
                               MaterialPageRoute(builder: (context) => const CadastroEmpresa()),
-
                             );
-
                           },
-
                           child: Text(
-
                             'Quero cadastrar\nminha empresa',
-
                             textAlign: TextAlign.center,
-
                             style: TextStyle(
-
                               fontSize: screenWidth * 0.055,
-
                               fontWeight: FontWeight.bold,
-
                               color: Colors.black,
-
                               decoration: TextDecoration.underline,
-
                               decorationThickness: 2.0,
-
                             ),
-
                           ),
-
                         ),
-
                       ),
-
-
-
                       SizedBox(height: screenHeight * 0.04),
-
                     ],
-
                   ),
-
                 ),
-
               ),
-
             ),
-
           ),
-
-
-
-          // Moldura superior
-
           Positioned(
-
             top: 0,
-
             left: 0,
-
             right: 0,
-
             child: SizedBox(
-
               height: molduraHeight,
-
               child: Image.asset(
-
                 'imagem/quebrasuperior.png',
-
                 width: double.infinity,
-
                 fit: BoxFit.cover,
-
                 alignment: Alignment.topCenter,
-
               ),
-
             ),
-
           ),
-
-
-
-          // Moldura inferior
-
           Positioned(
-
             bottom: 0,
-
             left: 0,
-
             right: 0,
-
             child: SizedBox(
-
               height: molduraHeight,
-
               child: Image.asset(
-
                 'imagem/quebrainferior.png',
-
                 width: double.infinity,
-
                 fit: BoxFit.cover,
-
                 alignment: Alignment.bottomCenter,
-
               ),
-
             ),
-
           ),
-
         ],
-
       ),
-
     );
-
   }
-
-
 
   Widget _buildTextField(String hint, TextEditingController controller, {bool isPassword = false}) {
-
     return Container(
-
       decoration: BoxDecoration(
-
         color: const Color(0xFFD1D5D8),
-
         borderRadius: BorderRadius.circular(25),
-
       ),
-
       child: TextFormField(
-
         controller: controller,
-
         obscureText: isPassword,
-
         decoration: InputDecoration(
-
           hintText: hint,
-
           hintStyle: const TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
-
           contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-
           border: InputBorder.none,
-
           suffixIcon: isPassword
-
               ? const Icon(Icons.visibility_outlined, color: Colors.black)
-
               : null,
-
         ),
-
       ),
-
     );
-
   }
-
 }
